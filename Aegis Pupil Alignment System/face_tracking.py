594c46fa0ca6fdcab149aea9e0e6d3b657033837
sambhabsau <sambhabsau@gmail.com>
2022-07-05 09:59:58 -0400

    Add files via upload

---
+++ Aegis Pupil Alignment System/face_tracking.py
@@ -0,0 +1,51 @@
+# Student: Raj Chaudhari
+# Student: Prashant Thirumal
+# Student: David Gonzalez
+# Student: Shom Prabhu
+# Student: Ibraiń Gonzalez
+
+# Description: Fall 2021 Capstore design Facial recognizition code for automated eye exam screenings.
+
+
+
+
+
+import cv2
+
+# Load the different cascades that are created using machinelearning algorithms
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#capturing video live detection
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+        
+# Release the VideoCapture object
+cap.release()
+
